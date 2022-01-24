b74b73b1842a9346758daaa2263c8e118154902b
dioexile <45012858+dioexile@users.noreply.github.com>
2022-01-24 04:39:13 +0300

    projects

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-`use strict`;
+const project = document.querySelector(".project");
+const resume = document.querySelector(".container");
+
 
 const circleRunWords = () => {
 	const text = document.querySelector(".profile_text p");
@@ -6,35 +8,17 @@ const circleRunWords = () => {
 	text.innerHTML = text.textContent.split("").map((char, i) => `<span style="transform: rotate(${i * 9.2}deg)">${char}</span>`).join("");
 }
 
+
+const showProject = () => {
+	project.style.display = 'block'
+	resume.style.display = 'none'
+}
+const showResume = () => {
+	project.style.display = 'none'
+	resume.style.display = 'grid'
+}
+
+
 circleRunWords();
-
-// убрано на время
-// const showProject = () => {
-// 	const mainResume = document.querySelector(".container");
-// 	const projectsResume = document.querySelector(".popup-container");
-
-// 	projectsResume.style.display = "flex";
-// 	setTimeout(() => {
-// 		mainResume.style.display = "none";
-// 		projectsResume.classList.add("active");
-// 	}, 800)
-// 	mainResume.classList.remove("active");
-// }
-
-// const showResume = () => {
-// 	const mainResume = document.querySelector(".container");
-// 	const projectsResume = document.querySelector(".popup-container");
-
-// 	setTimeout(() => {
-// 		projectsResume.style.display = "none";
-// 		mainResume.style.display = "grid";
-// 		setTimeout(() => {
-// 			mainResume.classList.add("active");
-// 		}, 200)
-// 	}, 800)
-// 	projectsResume.classList.remove("active");
-// }
-
-
-// document.querySelector(".button_text").addEventListener("click", showProject);
-// document.querySelector(".back").addEventListener("click", showResume);
+document.querySelector(".button_text").addEventListener("click", showProject);
+document.querySelector(".back").addEventListener("click", showResume);
