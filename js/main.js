a52fb0c2d657c2dd27de3faa956258151ffb462d
dioexile <45012858+dioexile@users.noreply.github.com>
2022-02-23 15:20:53 +0300

    2 page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const project = document.querySelector(".project");
 const resume = document.querySelector(".container");
-
+const project2 = document.querySelector(".project2");
+const page1 = document.getElementById('first-page');
+const page11 = document.getElementById('first-page2');
+const page2 = document.getElementById('second-page');
 
 const circleRunWords = () => {
 	const text = document.querySelector(".profile_text p");
@@ -16,9 +19,27 @@ const showProject = () => {
 const showResume = () => {
 	project.style.display = 'none'
 	resume.style.display = 'grid'
+	project2.style.display = 'none'
 }
+const showResume2 = () => {
+	project2.style.display = 'none'
+	resume.style.display = 'grid'
+}
+const showSecond = () => {
+	project.style.display = 'none'
+	project2.style.display = 'block'
+}
+const showFirst = () => {
+	project.style.display = 'block'
+	project2.style.display = 'none'
+	console.log('asd')
+}
+
+page2.addEventListener('click', showSecond);
+page11.addEventListener('click', showFirst);
 
 
 circleRunWords();
 document.querySelector(".button_text").addEventListener("click", showProject);
 document.querySelector(".back").addEventListener("click", showResume);
+document.querySelector(".back2").addEventListener("click", showResume2);
